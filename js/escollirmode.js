88b9dec0af092705f8b08019e245344819712b41
Nickdrem <songohanbiel@gmail.com>
2024-05-01 14:59:08 +0200

    sb

--- js/escollirmode.js
+++
@@ -1,5 +0,0 @@
-addEventListener('load', function() {
-    document.getElementById('select').addEventListener('click', function(){
-        window.location.assign("../../html/phasergame.html");
-    });
-});
